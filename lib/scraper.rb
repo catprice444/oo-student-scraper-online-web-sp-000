b2ddb6114aac621505fdbede104c687424e433eb
Catherine Price <catprice444@gmail.com>
2020-06-16 18:01:36 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,14 +2,48 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
+  #
+  # def self.scrape_index_page(index_url) #scraping the index page that lists all the students
+  #
+  #   doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"))
+  #   students = []
+  #
+  #     doc.css(".student-card").each do |student|
+  #       students_info = {}
+  #       students_info[:name] = student.css("h4.student-name").text
+  #       students_info[:location] = student.css("p.student-location").text
+  #       students_info[:profile_url] = student.css("a").attribute("href").value
+  #       students << students_info
+  #     end
+  #   students
+  # end
 
-  def self.scrape_index_page(index_url)
-    
+
+  def self.scrape_profile_page(profile_url) #responsible for scraping an individual student's profile page
+
+    profile = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/students/ryan-johnson.html"))
+    student_profile = []
+    # html = open(profile_url)
+    # profile = Nokogiri::HTML(html)
+    #
+    # profile.css("div.main-wrapper.profile .social-icon-container a").each do |social|
+    #   if social.attribute("href").value.include?("twitter")
+    #     student_profile[:twitter] = social.attribute("href").value
+    #   elsif social.attribute("href").value.include?("linkedin")
+    #     student_profile[:linkedin] = social.attribute("href").value
+    #   elsif social.attribute("href").value.include?("github").value
+    #     student_profile[:github] = social.attribute("href").value
+    #   else
+    #     student_profile[:blog] = social.attriute("href").value
+    #   end
+    # end
+    #
+    # student_profile[:profile_quote] = profile.css("div.main-wrapper.profile .vitals-text-container .profile-quote").text
+    # student_profile[:bio] =  profile.css("div.main-wrapper.profile .description-holder p").text
+    #
+    # student_profile
   end
 
-  def self.scrape_profile_page(profile_url)
-    
-  end
+  binding.pry
 
 end
-
