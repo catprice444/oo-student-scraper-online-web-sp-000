b1885c9d0266efe940c826848382b22f8c4887ff
Catherine Price <catprice444@gmail.com>
2020-06-16 18:06:51 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,27 +23,29 @@ class Scraper
 
     profile = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/students/ryan-johnson.html"))
     student_profile = []
-    # html = open(profile_url)
-    # profile = Nokogiri::HTML(html)
-    #
-    # profile.css("div.main-wrapper.profile .social-icon-container a").each do |social|
-    #   if social.attribute("href").value.include?("twitter")
-    #     student_profile[:twitter] = social.attribute("href").value
-    #   elsif social.attribute("href").value.include?("linkedin")
-    #     student_profile[:linkedin] = social.attribute("href").value
-    #   elsif social.attribute("href").value.include?("github").value
-    #     student_profile[:github] = social.attribute("href").value
-    #   else
-    #     student_profile[:blog] = social.attriute("href").value
-    #   end
-    # end
-    #
-    # student_profile[:profile_quote] = profile.css("div.main-wrapper.profile .vitals-text-container .profile-quote").text
-    # student_profile[:bio] =  profile.css("div.main-wrapper.profile .description-holder p").text
-    #
-    # student_profile
+    # 
   end
 
   binding.pry
 
 end
+
+# html = open(profile_url)
+# profile = Nokogiri::HTML(html)
+#
+# profile.css("div.main-wrapper.profile .social-icon-container a").each do |social|
+#   if social.attribute("href").value.include?("twitter")
+#     student_profile[:twitter] = social.attribute("href").value
+#   elsif social.attribute("href").value.include?("linkedin")
+#     student_profile[:linkedin] = social.attribute("href").value
+#   elsif social.attribute("href").value.include?("github").value
+#     student_profile[:github] = social.attribute("href").value
+#   else
+#     student_profile[:blog] = social.attriute("href").value
+#   end
+# end
+#
+# student_profile[:profile_quote] = profile.css("div.main-wrapper.profile .vitals-text-container .profile-quote").text
+# student_profile[:bio] =  profile.css("div.main-wrapper.profile .description-holder p").text
+#
+# student_profile
